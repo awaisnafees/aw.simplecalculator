37a20f8345f8d53b99c5370c8a452776b298030a
awaisnafees <awaisnafees96@gmail.com>
2020-06-26 12:09:19 -0700

    simple calculator ready

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(count){
+    document.getElementById("calbox").value+=count;
+}
+
+function cal(){
+document.getElementById("calbox").value=eval(document.getElementById("calbox").value);
+}
